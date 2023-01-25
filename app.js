cda4b3cfba935e3603f567eccabcb5a5b5150b7b
AbderrazekGraies <112821617+AbderrazekGraies@users.noreply.github.com>
2023-01-25 15:20:44 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -13,40 +13,40 @@ function startsign(){
         
 
     switch(signe){
-        case 0:
+        case 11:
             img.src = "img/Goat.jfif";
             break;
-        case 1:
+        case 0:
             img.src = "img/Monkey.jfif";
             break;
-        case 2:
+        case 1:
             img.src = "img/chicken.jfif";
             break;
-        case 3:
+        case 2:
             img.src = "img/dog.jfif";
             break;
-        case 4:
+        case 3:
             img.src = "img/pig.jfif";
             break;
-        case 5:
+        case 4:
             img.src = "img/Rat.jfif";
             break;
-        case 6:
+        case 5:
             img.src = "img/Buffalo.jfif";
             break;
-        case 7:
+        case 6:
             img.src = "img/tiger.jfif";
             break;
-        case 8:
+        case 7:
             img.src = "img/Rabbit.jfif";
             break;
-        case 9:
+        case 8:
             img.src = "img/dragon.jfif";
             break;
-        case 10:
+        case 9:
             img.src = "img/Snake.jfif";
             break;
-        case 11:
+        case 10:
             img.src = "img/Horse.png";
             break;
     }
